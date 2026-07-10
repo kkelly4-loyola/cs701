493f99cded2626ea70a8ac408f45f783fd227303
kkelly4-loyola <kkelly4@loyola.edu>
2026-07-10 09:14:10 -0400

    Triggering CI/CD build

---
+++ personal_info.py
@@ -0,0 +1,6 @@
+print("Name: Kristen Peake",
+"ID: 1914376",
+"Phone: 240-520-6628",
+"Department: Marketing and Communications",
+"Grade: Graduate",
+"Github link: https://github.com/kkelly4-loyola/cs701.git", sep="\n")
