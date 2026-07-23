52c4c6fd9583cfc6f9ceff7c006c4432ceac1bd3
kkelly4-loyola <kkelly4@loyola.edu>
2026-07-23 11:56:26 -0400

    it commit -m "Updated assignment"

---
+++ CH2/pa_01.py
@@ -0,0 +1,40 @@
+# Name: Kristen Peake
+# Course: CS701, Dr. Schneider
+# Date: 7/23/2026
+# Programming Assignment: 1
+# Program Inputs: Dimensions of the room (length, width, height) in feet
+# Program Outputs: Total area to be painted (in square feet), amount of primer (in gallons), amount of paint (in gallons)
+
+def main():
+    
+    '''This program Computes the amount of paint needed one more'''
+    # Step 1: Ask the user for the dimensions of the room
+    # The two dimensions are length & height.
+    
+
+    # Step 2: Compute the total area of the four walls and ceiling
+    # Area of walls:
+    
+    # Area of ceiling:
+
+
+    # Total area = Area of walls + Area of ceiling
+
+
+    # Step 3: Calculate the amount of primer and paint needed
+    # Primer coverage = 200 square feet per gallon
+
+
+    # Paint coverage = 350 square feet per gallon
+
+
+    # Step 4: Output the total area and the amount of primer and paint needed
+    # Uncomment the three lines below for the output
+    # print(f"Total area to be painted: {total_area:.2f} square feet")
+
+    # print(f"Amount of primer needed: {primer_needed:.2f} gallons")
+
+    # print(f"Amount of paint needed: {paint_needed:.2f} gallons")
+
+if __name__ == "__main__":
+    main()
